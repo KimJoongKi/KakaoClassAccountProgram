3c611aba2b428d30d28a5d13d7f6a8c2b594bfb2
KimJoongKi <thoug12@naver.com>
2022-01-03 22:45:46 +0900

    테스트 파일 mockMultipartFile 변경 테스트

--- src/test/java/com/kcap/kakaoclassaccountprogram/web/file/FileControllerTest.java
+++ src/test/java/com/kcap/kakaoclassaccountprogram/web/file/FileControllerTest.java
@@ -11,6 +11,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -27,22 +31,20 @@ class FileControllerTest {
 
     @Test
     public void 파일_업로드_테스트() throws Exception {
-        String fileName = "테스트파일";
-        String contentType = "xlsx";
-        String filePath = "src/main/resources/test/테스트파일.xlsx";
 
+        File file = new File("src/main/resources/test");  // 테스트 파일경로
+        Queue<MockMultipartFile> queue = new LinkedList<MockMultipartFile>();
+        File[] files = file.listFiles();
+        for (File file1 : files) {
+            MockMultipartFile mockMultipartFile =
+                    new MockMultipartFile(file1.getName(), new FileInputStream(file1));
+            queue.offer(new MockMultipartFile(file1.getName(), new FileInputStream(file1)));
+        }
 
+        // 테스트 파일 명에 맞게 변경
+        assertEquals("테스트파일.xlsx", queue.poll().getName());
+        assertEquals("테스트파일2.xlsx", queue.poll().getName());
 
-        FileInputStream fileInputStream =
-                new FileInputStream(new File(filePath));
-        MockMultipartFile mockMultipartFile =
-                new MockMultipartFile(fileName,
-                        fileName + "." + contentType,
-                        contentType,
-                        fileInputStream);
-
-        assertEquals(fileName, mockMultipartFile.getName());
-        assertThat(mockMultipartFile.getOriginalFilename(), is(fileName + "."+ contentType));
     }
 
 }
