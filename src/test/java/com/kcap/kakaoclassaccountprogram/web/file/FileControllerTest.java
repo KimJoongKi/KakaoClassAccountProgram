fed4f451c7b1eb28b6848fe8f666eeaeba738c61
KimJoongKi <thoug12@naver.com>
2022-01-07 13:31:48 +0900

    카카오뱅크 엑셀파일 검증 테스트 추가

--- src/test/java/com/kcap/kakaoclassaccountprogram/web/file/FileControllerTest.java
+++ src/test/java/com/kcap/kakaoclassaccountprogram/web/file/FileControllerTest.java
@@ -24,6 +24,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -56,20 +58,31 @@ class FileControllerTest {
 
     }
 
-    // TODO: 2022/01/03 엑셀파일 검증과정 추가
     @Test
     public void 카카오뱅크_엑셀파일_검증_테스트() throws IOException {
         File file = new File("src/main/resources/test");  // 테스트 파일경로
         FileInputStream fis = new FileInputStream(file.listFiles()[0]);
         XSSFWorkbook excel = new XSSFWorkbook(fis);;
         String sheetName = excel.getSheetName(0);
-        String cellName = excel.getSheetAt(0)
-                .getRow(0)
-                .getCell(1)
-                .toString();
+        String cellName = getCellDate(excel, 0, 1);
+        String accountHolder = getCellDate(excel, 3, 2);
+        String accountNumber = getCellDate(excel, 4, 2);
+        Pattern pattern = Pattern.compile("([0-9])+");
+        Matcher matcher = pattern.matcher(accountNumber);
+        matcher.find();
 
         assertEquals("카카오뱅크 거래내역", sheetName, cellName);
+        assertEquals("김중기", accountHolder);
+        assertEquals("1826", matcher.group());
+    }
 
+    // TODO: 2022/01/07 엑셀파일 데이터 입력 테스트 작업
+
+    private String getCellDate(XSSFWorkbook excel, int row, int cell) {
+        return excel.getSheetAt(0)
+                .getRow(row)
+                .getCell(cell)
+                .toString();
     }
 
 }
