22c508c63138a013cd4ed16ba11b2373c64010dc
KimJoongKi <thoug12@naver.com>
2022-02-21 23:59:10 +0900

    파일 업로드 폼 작성

--- src/main/java/com/kcap/kakaoclassaccountprogram/web/file/FileController.java
+++ src/main/java/com/kcap/kakaoclassaccountprogram/web/file/FileController.java
@@ -1,9 +1,12 @@
 package com.kcap.kakaoclassaccountprogram.web.file;
 
+import com.kcap.kakaoclassaccountprogram.web.file.form.FileForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Controller
 @RequestMapping("/file")
 public class FileController {
@@ -15,16 +18,16 @@ public class FileController {
     }
 
     @GetMapping("/upload")
-    public String upload() {
-        return "upload";
+    public String updateView(@ModelAttribute FileForm form) {
+        return "file/upload";
     }
 
     @PostMapping("/upload")
-    public String upload(@RequestParam("file") MultipartFile[] files) {
-        // TODO: 2022/02/06 file upload data insert
-
+    @ResponseBody
+    public String upload(@ModelAttribute FileForm form) {
+        // TODO: 2022/02/06 file upload data insert 와 진짜 한글이 이렇게 빨라지다...
         // TODO: 2022/02/06 excel data insert (path)
         // TODO: 2022/02/06 excel etc data insert
-        return "redirect:/";
+        return "file/upload";
     }
 }
---
+++ src/main/java/com/kcap/kakaoclassaccountprogram/web/file/form/FileForm.java
@@ -0,0 +1,12 @@
+package com.kcap.kakaoclassaccountprogram.web.file.form;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter @Setter
+public class FileForm {
+    private List<MultipartFile> uploadFiles;
+}
